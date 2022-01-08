5e02c8b165308098dc0b0619253cbdd807b8d0ea
Ekrem Bayram <37544619+ekrem007@users.noreply.github.com>
2022-01-08 19:38:27 +0300

    Add proforma table

---
+++ src/proforma.js
@@ -0,0 +1,299 @@
+const proformaOverlay = document.getElementById('proformaOverlay')
+const proformaOverlayIcon = proformaOverlay.getElementsByClassName('iconify')
+const proformaOverlayText = proformaOverlay.querySelector('h3')
+
+const proformaTabPage = proformaOverlay.parentElement
+proformaTabPage.stopLoadingContent = () => {
+    stopProformaCurrentQuery()
+    stopProformaCurrentQuery = () => { }
+    proformaCurrentQuery = undefined
+    currentProformaRefQueries.forEach(stopRefQuery => stopRefQuery())
+    currentProformaRefQueries = []
+}
+proformaTabPage.loadContent = () => {
+    proformaTabPage.stopLoadingContent()
+    loadProforma()
+}
+
+const proformaTable = proformaTabPage.querySelector('table#proforma')
+proformaTable.parentElement.onscroll = () => inlineEdit.moveToAnchor()
+const proformaList = proformaTable.querySelector('tbody#proformaList')
+let proformaCurrentOrder, proformaCurrentOrderDirection
+
+const proformaColumnsJSON = require('./proformaColumns.json')
+const proformaHeadersList = proformaTable.querySelector('#proformaHeadersList')
+const proformaHeaderTemplate = document.getElementById('proformaHeaderTemplate')
+
+function newProformaHeader(headerID) {
+    const th = proformaHeaderTemplate.content.firstElementChild.cloneNode(true)
+    new MDCRipple(th)
+    th.id = headerID
+
+    th.onmousedown = mouseEvent => {
+        if (mouseEvent.button == 0) {
+            if (th.parentElement != proformaHeadersList) {
+            }
+        }
+    }
+    th.onmouseup = () => {
+        if (th.parentElement != proformaHeadersList) {
+            if (proformaList.childElementCount > 0) {
+                setProformaOverlayState('hide')
+            }
+            else {
+                setProformaOverlayState('empty')
+            }
+        }
+    }
+    th.onclick = () => proformaHeaderClick(headerID)
+
+    const label = th.querySelector('label')
+    label.textContent = translate(proformaColumnsJSON[headerID])
+
+    th.sortIcon = th.getElementsByClassName('iconify')
+
+    return th
+}
+
+function loadProformaColumns() {
+    setProformaOverlayState('loading')
+
+    let columns = Object.keys(proformaColumnsJSON)
+    if (localStorage.getItem('proformaColumns')) {
+        columns = localStorage.getItem('proformaColumns').split(',')
+    }
+    columns.forEach(headerID => proformaHeadersList.appendChild(newProformaHeader(headerID)))
+
+    if (proformaHeadersList.children['name']) {
+        proformaHeaderClick('name')
+        proformaHeaderClick('name')
+    }
+    else {
+        proformaHeaderClick(proformaHeadersList.firstChild.id)
+    }
+}
+
+loadProformaColumns()
+
+let proformaCurrentQuery
+let proformaCurrentProformaSnap
+let stopProformaCurrentQuery = () => { }
+let currentProformaRefQueries = []
+let selectedProforma, selectedProformaRow, selectedProformaID
+
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        if (proformaTabPage.classList.contains('show')) {
+            proformaTabPage.loadContent()
+        }
+    }
+    else {
+        proformaTabPage.stopLoadingContent()
+    }
+})
+
+function proformaHeaderClick(headerID) {
+    const clickedHeader = proformaHeadersList.querySelector('th#' + headerID)
+    if (clickedHeader) {
+        proformaHeadersList.querySelectorAll('[data-icon="ic:round-keyboard-arrow-up"]').forEach(otherHeaderIcon => {
+            if (otherHeaderIcon.parentElement != clickedHeader) {
+                otherHeaderIcon.classList.remove('rot-180')
+                otherHeaderIcon.setAttribute('data-icon', 'ic:round-unfold-more')
+            }
+        })
+
+        if (clickedHeader.sortIcon[0].getAttribute('data-icon') == 'ic:round-unfold-more') {
+            clickedHeader.sortIcon[0].setAttribute('data-icon', 'ic:round-keyboard-arrow-up')
+        }
+
+        if (clickedHeader.sortIcon[0].classList.contains('rot-180')) {
+            orderProforma(headerID, 'asc')
+        }
+        else {
+            orderProforma(headerID, 'desc')
+        }
+
+        clickedHeader.sortIcon[0].classList.toggle('rot-180')
+    }
+}
+
+function loadProforma() {
+    if (selectedCase) {
+        proformaCurrentQuery = selectedCase.collection('proforma')
+        stopProformaCurrentQuery = proformaCurrentQuery.onSnapshot(
+            snapshot => {
+                console.log(snapshot)
+                proformaCurrentProformaSnap = snapshot
+                listProforma(snapshot)
+            },
+            error => {
+                console.error('Error getting proforma: ' + error)
+                setProformaOverlayState('empty')
+            }
+        )
+    }
+    else {
+        stopProformaCurrentQuery()
+        stopProformaCurrentQuery = () => { }
+        proformaCurrentQuery = undefined
+        setProformaOverlayState('empty')
+    }
+}
+
+function listProforma(snap) {
+    if (snap.docs.length > 0) {
+        proformaList.innerHTML = ''
+        currentProformaRefQueries.forEach(stopRefQuery => stopRefQuery())
+        currentProformaRefQueries = []
+        snap.forEach(Proformanap => {
+            setProformaOverlayState('hide')
+
+            const tr = document.createElement('tr')
+            tr.id = Proformanap.id
+            tr.onmousedown = mouseEvent => {
+                if (mouseEvent.button != 1) {
+                    if (selectedProformaID != Proformanap.id) {
+                        if (selectedProformaRow) {
+                            selectedProformaRow.classList.remove('selected')
+                        }
+                        selectedProforma = proformaCurrentQuery.doc(Proformanap.id)
+                        selectedProformaID = Proformanap.id
+                        selectedProformaRow = tr
+                        selectedProformaRow.classList.add('selected')
+                    }
+                }
+            }
+            tr.onmouseup = mouseEvent => {
+                if (getSelectedText() != '') {
+                    textContextMenu.style.left = (mouseEvent.clientX) + 'px'
+                    textContextMenu.style.top = (mouseEvent.clientY) + 'px'
+                    textContextMenu.materialComponent.setAbsolutePosition((mouseEvent.clientX), (mouseEvent.clientY))
+                    textContextMenu.materialComponent.open = true
+                }
+                else if (mouseEvent.button == 2) {
+                    proformaContextMenu.style.left = (mouseEvent.clientX) + 'px'
+                    proformaContextMenu.style.top = (mouseEvent.clientY) + 'px'
+                    proformaContextMenu.materialComponent.setAbsolutePosition((mouseEvent.clientX), (mouseEvent.clientY))
+                    proformaContextMenu.materialComponent.open = true
+                }
+            }
+            if (tr.id == selectedProformaID) {
+                selectedProforma = proformaCurrentQuery.doc(selectedProformaID)
+                selectedProformaRow = tr
+                selectedProformaRow.classList.add('selected')
+            }
+            proformaList.appendChild(tr)
+
+            for (const column of proformaHeadersList.children) {
+                const td = document.createElement('td')
+                td.id = column.id
+                tr.appendChild(td)
+
+                if (td.id == '__name__') {
+                    td.textContent = Proformanap.id
+                }
+                else {
+                    const value = Proformanap.get(td.id)
+                    if (value != undefined) {
+                        if (td.id.includes('Date')) {
+                            td.textContent = new Date(value.seconds * 1000).toLocaleString()
+                        }
+                        else if (typeof value === 'object' && value !== null) {
+                            currentProformaRefQueries.push(
+                                value.onSnapshot(
+                                    snapshot => {
+                                        td.textContent = snapshot.get('name')
+                                        orderProforma(proformaCurrentOrder, proformaCurrentOrderDirection)
+                                    },
+                                    error => {
+                                        console.error(error)
+                                    }
+                                )
+                            )
+                        }
+                        else {
+                            td.textContent = value
+                        }
+                    }
+                }
+                if (td.id == 'name') {
+                    td.onclick = () => {
+                        const proformaName = td.textContent
+                        inlineEdit.show(td, Proformanap.ref.path, proformaName.slice(0, proformaName.lastIndexOf('.')))
+                        inlineEditInput.proformaType = proformaName.slice(proformaName.lastIndexOf('.')).toLowerCase()
+                    }
+                }
+            }
+        })
+        orderProforma(proformaCurrentOrder, proformaCurrentOrderDirection)
+    }
+    else {
+        setProformaOverlayState('empty')
+    }
+}
+
+function orderProforma(orderBy, orderDirection) {
+    let switching, i, shouldSwitch
+    do {
+        switching = false
+        for (i = 0; i < proformaList.childElementCount - 1; i++) {
+            shouldSwitch = false
+
+            const a = proformaList.children[i].children[orderBy]
+            const b = proformaList.children[i + 1].children[orderBy]
+
+            if (orderDirection == 'asc') {
+                if (a.innerHTML.toLowerCase() > b.innerHTML.toLowerCase()) {
+                    shouldSwitch = true
+                    break
+                }
+            }
+            else if (orderDirection == 'desc') {
+                if (a.innerHTML.toLowerCase() < b.innerHTML.toLowerCase()) {
+                    shouldSwitch = true
+                    break
+                }
+            }
+        }
+        if (shouldSwitch) {
+            proformaList.children[i].parentElement.insertBefore(proformaList.children[i + 1], proformaList.children[i])
+            switching = true
+        }
+    }
+    while (switching)
+
+    proformaCurrentOrder = orderBy
+    proformaCurrentOrderDirection = orderDirection
+}
+
+function setProformaOverlayState(state) {
+    switch (state) {
+        case 'loading':
+            proformaOverlay.classList.remove('hide')
+            proformaOverlay.classList.remove('show-headers')
+            proformaOverlayIcon[0].setAttribute('data-icon', 'eos-icons:loading')
+            proformaOverlayText.hidden = true
+            break
+        case 'empty':
+            proformaOverlay.classList.remove('hide')
+            proformaOverlay.classList.remove('show-headers')
+            proformaOverlayIcon[0].setAttribute('data-icon', 'ic:round-sentiment-dissatisfied')
+            proformaOverlayText.hidden = false
+            proformaOverlayText.innerText = translate('ACTIVITIES') + ' ' + translate('NOT_FOUND')
+            break
+        case 'hide':
+            proformaOverlay.classList.add('hide')
+            break
+        default:
+            break
+    }
+}
+
+function refreshAndSaveProformaColumns() {
+    listProforma(proformaCurrentProformaSnap)
+    let proformaColumns = []
+    for (const header of proformaHeadersList.children) {
+        proformaColumns.push(header.id)
+    }
+    localStorage.setItem('proformaColumns', proformaColumns)
+}
